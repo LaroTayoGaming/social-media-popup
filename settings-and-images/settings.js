4d2019d340e65684f2647d5a7952073246612ae7
One and only LTG <larotayogaming@protonmail.com>
2024-09-07 11:54:14 +0800

    Update settings.js

--- settings-and-images/settings.js
+++ settings-and-images/settings.js
@@ -1,7 +1,7 @@
 var settings = {
     "options": {
         "pauseTime": "6",
-        "inbetweenPauseTime": "100"
+        "inbetweenPauseTime": "15"
     },
     "colors": {
         "iconBoxColor": "rgba(185,0,187,1)",
